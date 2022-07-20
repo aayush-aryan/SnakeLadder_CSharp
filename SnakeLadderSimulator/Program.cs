4940cafbd553f2cb0cf00b55d92071a9b6ae2e33
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-21 03:22:13 +0530

    added. a method for rolling die

--- SnakeLadderSimulator/Program.cs
+++ SnakeLadderSimulator/Program.cs
@@ -7,6 +7,8 @@ namespace SnakeLadderSimulator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To SnakeLadder:");
+
+            
         }
     }
 }
--- SnakeLadderSimulator/SnakeLadderGame.cs
+++ SnakeLadderSimulator/SnakeLadderGame.cs
@@ -7,5 +7,14 @@ namespace SnakeLadderSimulator
     class SnakeLadderGame
     {
         int Position = 0;
+        /// <summary>
+        /// rolling die Method
+        /// </summary>
+        /// <returns></returns>
+        static int RollingDie()
+        {
+            Random random = new Random();
+            return random.Next(1, 7); ;
+        }
     }
 }
